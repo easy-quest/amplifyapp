37bc6486fc49865d0c849a560a9f003cae413f02
easy-quest <easy.quest@yandex.ru>
2021-05-09 13:54:38 +0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Easy Quest V2</h1>
+        <h1>Easy Quest V3</h1>
       </header>
     </div>
   );
